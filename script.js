5d48d45d7573dc73b91c1211979479754f9ce768
PalomaBayon <bayonpaloma@gmail.com>
2026-09-23 19:54:01 -0300

    agregar menu

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// menu
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+const menuOverlay = document.getElementById("menuOverlay");
+const menuLinks = document.querySelectorAll(".nav-menu a");
+
+function toggleMenu() {
+  navMenu.classList.toggle("active");
+  menuOverlay.classList.toggle("active");
+}
+
+function closeMenu() {
+  navMenu.classList.remove("active");
+  menuOverlay.classList.remove("active");
+}
+
+menuToggle.addEventListener("click", (event) => {
+  toggleMenu();
+  event.stopPropagation();
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
+
+menuOverlay.addEventListener("click", () => {
+  closeMenu();
+});
+// popup formulario
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".contacto form");
   const popup = document.getElementById("customPopup");
